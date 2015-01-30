ec577133ceb31c2dbefb70b0233e972e8df89f7c
alesuarez <sas.tuc@gmail.com>
2015-01-30 11:03:11 -0300

    comenzando TDD

---
+++ TDD/src/com/javaboot/tdd/TestRecentFile.java
@@ -0,0 +1,14 @@
+package com.javaboot.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRecentFile {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
