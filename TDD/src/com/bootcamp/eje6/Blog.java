b4e9c579ad8da23af37ba012872d53b45ad3e4a9
alesuarez <sas.tuc@gmail.com>
2015-01-30 17:47:03 -0300

    eje6 tdd

--- TDD/src/com/bootcamp/eje6/Blog.java
+++ TDD/src/com/bootcamp/eje6/Blog.java
@@ -16,5 +16,59 @@ public class Blog {
 		// TODO Auto-generated method stub
 		System.out.println(this.entry.get(i).toString());		
 	}
+	public void deleteEntry(String title) {
+		// TODO Auto-generated method stub
+		
+		boolean band=false;
+		
+		for(int j=0;j<entry.size();j++){
+			if (title.equalsIgnoreCase(entry.get(j).getTitle())){
+				entry.remove(j);
+				System.out.println("Entrada borrada con exito");
+				band=true;
+			}
+				
+		}
+		if (!band) System.out.println("Entrada no encontrada");		
+		
+		
+	}
+	public void viewAllEntry() {
+		// TODO Auto-generated method stub
+		System.out.println(entry);
+		
+	}
+	public void updateEntry(String title, String content) {
+		// TODO Auto-generated method stub
+		boolean band=false;
+		for(int j=0;j<entry.size();j++){
+			if (title.equalsIgnoreCase(entry.get(j).getTitle())){
+				entry.get(j).setContent(content);
+				System.out.println("Entrada modificada con exito");
+				band=true;
+			}
+				
+		}
+		if (!band) System.out.println("Entrada no encontrada");	
+		
+	}
+	public void viewMostRecent() {
+		// TODO Auto-generated method stub
+		int size=entry.size();
+		int i;
+		int contador=10;
+		for (i=size;i>=0;i--){
+			System.out.println(entry.get(i-1).toString());
+			contador--;
+			if (contador==0) i=-1;
+		}
+		
+		
+	}
+	public void viewTagAll() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	
 }
